0b1742fa97d43d4c835be2a3d8827ffd3833414f
unknown <loganathan.arumugam@TWGC01A609.wearefmg.net>
2017-09-26 01:36:56 +0530

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginModule } from "./login/login.module";
+import { SignupModule } from "./signup/signup.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    SignupModule
   ],
   providers: [],
   bootstrap: [AppComponent]
